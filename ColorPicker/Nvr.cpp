f626a4d8dc6816b15075ac1c6f8dcdd291ba0d1d
Carlo Wood <carlo@alinoe.com>
2023-04-02 15:32:50 +0200

    Quick hack to allow using this for colors in .css files too.
    Usage: ./colorpicker some.css:42
    
    where line 42 contains the color code to be changed.

--- ColorPicker/Nvr.cpp
+++ ColorPicker/Nvr.cpp
@@ -18,19 +18,15 @@ void Nvr::update_color(QColor const& color)
   color.getRgb(&R, &G, &B);
   std::stringstream scolor;
   scolor << '#' << std::hex << std::setfill('0') << std::setw(2) << R << std::setw(2) << G << std::setw(2) << B;
-  std::string remote_command = ":hi " + m_syntax_group;
-  if (m_syntax_group.find("Error") != std::string::npos || m_syntax_group == "Search")
-    remote_command += " guibg=";
-  else
-    remote_command += " guifg=";
-  remote_command += scolor.str() + "<CR>";
-  std::string command = "nvr --nostart --servername \"";
-  command += NVIM_LISTEN_ADDRESS;
-  command += "\" --remote-send \"";
-  command += remote_command;
-  command += "\"";
-  //std::cout << "Executing command \"" << remote_command << "\"." << std::endl;
-  int status = system(command.c_str());
+  // m_syntax_group is the first argument passed to colorpicker and should
+  // be the .css file plus a color plus the line number that should be changed.
+  auto colon = m_syntax_group.rfind(':');
+  int line = std::stoi(m_syntax_group.substr(colon + 1));
+  std::string file = m_syntax_group.substr(0, colon);
+  std::ostringstream remote_command;
+  remote_command << "sed -i -r -e '" << line << "s/#[0-9a-fA-F]{3,6}/" << scolor.str() << "/' " << file;
+  std::cout << "Executing command \"" << remote_command.str() << "\"." << std::endl;
+  int status = system(remote_command.str().c_str());
   if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
-    std::cerr << "Failure to send \"" << command << "\" to nvim!" << std::endl;
+    std::cerr << "Failure to exec \"" << remote_command.str() << "\"!" << std::endl;
 }
