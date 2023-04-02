43ec5c8d07841bd7acfa96631d28087d6b435089
Carlo Wood <carlo@alinoe.com>
2023-04-02 16:07:20 +0200

    Fix syntex hilighting for Underlined.
    Hardcoded guibg for Underlined, at the moment.

--- ColorPicker/Nvr.cpp
+++ ColorPicker/Nvr.cpp
@@ -19,7 +19,7 @@ void Nvr::update_color(QColor const& color)
   std::stringstream scolor;
   scolor << '#' << std::hex << std::setfill('0') << std::setw(2) << R << std::setw(2) << G << std::setw(2) << B;
   std::string remote_command = ":hi " + m_syntax_group;
-  if (m_syntax_group.find("Error") != std::string::npos || m_syntax_group == "Search")
+  if (m_syntax_group.find("Error") != std::string::npos || m_syntax_group == "Search" || m_syntax_group == "Underlined")
     remote_command += " guibg=";
   else
     remote_command += " guifg=";
@@ -29,7 +29,7 @@ void Nvr::update_color(QColor const& color)
   command += "\" --remote-send \"";
   command += remote_command;
   command += "\"";
-  //std::cout << "Executing command \"" << remote_command << "\"." << std::endl;
+  std::cout << "Executing command \"" << remote_command << "\"." << std::endl;
   int status = system(command.c_str());
   if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
     std::cerr << "Failure to send \"" << command << "\" to nvim!" << std::endl;
